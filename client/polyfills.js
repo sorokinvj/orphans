58b9f3637b90d9b061084b84da882369a38d3bc6
Vladislav Sorokin <sorokinvj@gmail.com>
2019-04-25 00:49:15 +0100

    no polyfills

--- client/polyfills.js
+++ client/polyfills.js
@@ -1,23 +1,23 @@
 /* eslint no-extend-native: 0 */
 // core-js comes with Next.js. So, you can import it like below
-import includes from 'core-js/library/fn/string/virtual/includes'
-import repeat from 'core-js/library/fn/string/virtual/repeat'
+// import includes from 'core-js/library/fn/string/virtual/includes'
+// import repeat from 'core-js/library/fn/string/virtual/repeat'
 
 
 // Add your polyfills
 // import 'core-js/es6/map';
 // import 'core-js/es6/set';
 
-import 'core-js/fn/object/assign';
+// import 'core-js/fn/object/assign';
 // import 'core-js/fn/string/ends-with';
 // import 'core-js/fn/string/starts-with';
 // import 'core-js/fn/string/includes';
-import 'core-js/fn/array/includes';
+// import 'core-js/fn/array/includes';
 // import 'core-js/fn/array/find';
 
 // import 'core-js/fn/number/is-finite';
 // This files runs at the very beginning (even before React and Next.js core)
 // console.log('Load your polyfills')
 
-String.prototype.includes = includes
-String.prototype.repeat = repeat
+// String.prototype.includes = includes
+// String.prototype.repeat = repeat
