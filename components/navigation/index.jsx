38110786a70f333379601780da8b9d994e9273ce
Vladislav Sorokin <sorokinvj@gmail.com>
2020-03-15 23:53:37 +0000

    fix locale and eslint

--- components/navigation/index.jsx
+++ components/navigation/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { withRouter } from 'next/router';
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { withRouter } from 'next/router'
 
-import { Container } from '@bootstrap-styled/v4';
-import Popup from 'reactjs-popup';
-import Logo from './Logo';
-import Burger from './Burger';
-import Menu from './Menu';
-import LangSelector from './LangSelector';
+import { Container } from '@bootstrap-styled/v4'
+import Popup from 'reactjs-popup'
+import Logo from './Logo'
+import Burger from './Burger'
+import Menu from './Menu'
+import LangSelector from './LangSelector'
 
 const contentStyle = {
   position: 'relative',
@@ -18,7 +18,7 @@ const contentStyle = {
   margin: '0',
   border: '0',
   padding: '0',
-};
+}
 
 const Navbar = styled.nav`
   height: 7rem;
@@ -28,7 +28,8 @@ const Navbar = styled.nav`
   position: absolute;
   top: 0;
   width: 100%;
-  border-bottom: ${props => (props.border ? '1px solid rgba(0,0,0,0.13)' : 'none')};
+  border-bottom: ${props =>
+    props.border ? '1px solid rgba(0,0,0,0.13)' : 'none'};
 
   .container {
     display: flex;
@@ -36,7 +37,7 @@ const Navbar = styled.nav`
     justify-content: space-between;
   }
 
-  @media (max-width:991px) {
+  @media (max-width: 991px) {
     .container > .menu {
       display: none;
     }
@@ -45,17 +46,16 @@ const Navbar = styled.nav`
   .popup-overlay {
     height: 50vh;
   }
-`;
+`
 
 const Nav = ({ router }) => (
-
-  <Navbar border={(router.route !== '/') || (router.route !== '/about')}>
+  <Navbar border={router.route !== '/' || router.route !== '/about'}>
     {/* {console.log('nav', router.route === '/about')} */}
     <Container>
-      <Logo white={(router.route === '/') || (router.route === '/about')} />
+      <Logo white={router.route === '/' || router.route === '/about'} />
       <Menu />
       {/* убрал переключатель языка до появления переводов */}
-      {/* <LangSelector white={router.route === '/' || router.route === '/about'} /> */}
+      <LangSelector white={router.route === '/' || router.route === '/about'} />
       {/* burger button and mobile menu */}
       <Popup
         modal
@@ -66,21 +66,19 @@ const Nav = ({ router }) => (
         {close => <Menu close={close} />}
       </Popup>
     </Container>
-
   </Navbar>
-
-);
+)
 
 Nav.propTypes = {
   router: PropTypes.shape({
     route: PropTypes.string,
   }),
-};
+}
 
 Nav.defaultProps = {
   router: {
     route: '/',
   },
-};
+}
 
-export default withRouter(Nav);
+export default withRouter(Nav)
