ec35826ed3f50a1fae48d0886d4e1f8c5f8d0f6b
Vladislav Sorokin <sorokinvj@gmail.com>
2020-03-16 00:20:33 +0000

    English version (#10)
    * fix locale and eslint
    
    * fix about head
    
    * added pic to investigations

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import getConfig from 'next/config';
-import { withRouter } from 'next/router';
-import Prismic from 'prismic-javascript';
-import { client } from '../prismic-configuration';
+import React from 'react'
+import PropTypes from 'prop-types'
+import getConfig from 'next/config'
+import { withRouter } from 'next/router'
+import Prismic from 'prismic-javascript'
+import { client } from '../prismic-configuration'
 
-import LanguageContext from '../components/context/LanguageContext';
-import RussiaMap from '../components/map/RussiaMap';
-import AboutHead from '../components/about/AboutHead';
-import AboutPage from '../components/about';
+import LanguageContext from '../components/context/LanguageContext'
+import RussiaMap from '../components/map/RussiaMap'
+import AboutHead from '../components/about/AboutHead'
+import AboutPage from '../components/about'
 
-const { publicRuntimeConfig } = getConfig();
+const { publicRuntimeConfig } = getConfig()
 
 class About extends React.Component {
-  static contextType = LanguageContext;
+  static contextType = LanguageContext
 
   static propTypes = {
     phone: PropTypes.string,
@@ -22,43 +22,43 @@ class About extends React.Component {
         description: PropTypes.arrayOf(
           PropTypes.shape({
             text: PropTypes.string,
-          }),
+          })
         ),
         team: PropTypes.arrayOf(
           PropTypes.shape({
             name: PropTypes.arrayOf(
               PropTypes.shape({
                 text: PropTypes.string,
-              }),
+              })
             ),
             position: PropTypes.arrayOf(
               PropTypes.shape({
                 text: PropTypes.string,
-              }),
+              })
             ),
             userpic: PropTypes.shape({
               url: PropTypes.string,
             }),
-          }),
+          })
         ),
         team_description: PropTypes.arrayOf(
           PropTypes.shape({
             text: PropTypes.string,
-          }),
+          })
         ),
         team_heading: PropTypes.arrayOf(
           PropTypes.shape({
             text: PropTypes.string,
-          }),
+          })
         ),
         title: PropTypes.arrayOf(
           PropTypes.shape({
             text: PropTypes.string,
-          }),
+          })
         ),
       }),
     }),
-  };
+  }
 
   static defaultProps = {
     page: {},
@@ -67,51 +67,51 @@ class About extends React.Component {
 
   state = {
     currentLang: null,
-  };
+  }
 
   static async getInitialProps(ctx) {
     const {
       query: { lang },
-    } = ctx;
+    } = ctx
 
     const page = await client.query(
       Prismic.Predicates.at('document.type', 'about'),
       {
         lang,
-      },
-    );
+      }
+    )
 
-    return { page: page.results[0] };
+    return { page: page.results[0] }
   }
 
   async componentDidMount() {
-    const language = this.context;
+    const language = this.context
     this.setState({
       currentLang: language,
-    });
+    })
   }
 
   async componentDidUpdate() {
-    const { currentLang } = this.state;
-    const { router } = this.props;
-    const language = this.context;
+    const { currentLang } = this.state
+    const { router } = this.props
+    const language = this.context
     if (language !== currentLang) {
-      router.push(`/${language}/about`);
+      router.push(`/${language}/about`)
     }
   }
 
   render() {
-    const { MapboxToken } = publicRuntimeConfig;
-    const { phone, page } = this.props;
-    const lang = this.context;
+    const { MapboxToken } = publicRuntimeConfig
+    const { phone, page } = this.props
+    const lang = this.context
     return (
       <>
-        <AboutHead />
+        <AboutHead lang={lang} />
         <RussiaMap token={MapboxToken} />
         <AboutPage page={page} phone={phone} lang={lang} />
       </>
-    );
+    )
   }
 }
 
-export default withRouter(About);
+export default withRouter(About)
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import getConfig from 'next/config';
-import { withRouter } from 'next/router';
-import Prismic from 'prismic-javascript';
-import { client } from '../prismic-configuration';
+import React from 'react'
+import getConfig from 'next/config'
+import { withRouter } from 'next/router'
+import Prismic from 'prismic-javascript'
+import { client } from '../prismic-configuration'
 
-import LanguageContext from '../components/context/LanguageContext';
-import RussiaMap from '../components/map/RussiaMap';
-import MainHead from '../components/main/MainHead';
-import MainPage from '../components/main';
+import LanguageContext from '../components/context/LanguageContext'
+import RussiaMap from '../components/map/RussiaMap'
+import MainHead from '../components/main/MainHead'
+import MainPage from '../components/main'
 
-const { publicRuntimeConfig } = getConfig();
+const { publicRuntimeConfig } = getConfig()
 
 class Index extends React.Component {
   static contextType = LanguageContext
@@ -25,38 +25,38 @@ class Index extends React.Component {
   }
 
   async componentDidMount() {
-    const language = this.context;
+    const language = this.context
     this.setState({
       currentLang: language,
-    });
+    })
     const stories = await client.query(
       Prismic.Predicates.at('document.type', 'post'),
       {
         lang: language,
         orderings: '[document.first_publication_date]',
-      },
-    );
+      }
+    )
     const investigations = await client.query(
       Prismic.Predicates.at('document.type', 'investigation'),
       {
         lang: language,
         orderings: '[my.investigation.priority]',
-      },
-    );
+      }
+    )
     const videos = await client.query(
       Prismic.Predicates.at('document.type', 'video'),
       {
         lang: language,
         orderings: '[document.first_publication_date desc]',
-      },
-    );
+      }
+    )
     const context = await client.query(
       Prismic.Predicates.at('document.type', 'context'),
       {
         lang: language,
         orderings: '[document.first_publication_date desc]',
-      },
-    );
+      }
+    )
     this.setState({
       content: {
         stories: stories.results,
@@ -64,26 +64,24 @@ class Index extends React.Component {
         videos: videos.results,
         context: context.results,
       },
-    });
+    })
   }
 
   async componentDidUpdate() {
-    const { currentLang } = this.state;
-    const { router } = this.props;
-    const language = this.context;
+    const { currentLang } = this.state
+    const { router } = this.props
+    const language = this.context
     if (language !== currentLang) {
-      router.push(`/${language}`);
+      router.push(`/${language}`)
     }
   }
 
   render() {
-    const { MapboxToken } = publicRuntimeConfig;
-    const { content } = this.state;
-    const {
-      stories, investigations, videos, context,
-    } = content;
-    const { phone } = this.props;
-    const lang = this.context;
+    const { MapboxToken } = publicRuntimeConfig
+    const { content } = this.state
+    const { stories, investigations, videos, context } = content
+    const { phone } = this.props
+    const lang = this.context
     // console.log('main', this.state);
     return (
       <>
@@ -98,8 +96,8 @@ class Index extends React.Component {
           lang={lang}
         />
       </>
-    );
+    )
   }
 }
 
-export default withRouter(Index);
+export default withRouter(Index)
